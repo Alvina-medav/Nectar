8105522f4bcba34e838d60093699e3b518b39b61
alvina-medav <alvinamed25@gmail.com>
2025-01-24 19:57:02 +0530

    commit changes

---
+++ Nectar/View/LoginInView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginInView.swift
+//  Nectar
+//
+//  Created by students on 4/11/46.
+//
+
+import SwiftUI
+
+struct LoginInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginInView()
+}
---
+++ Nectar/View/SIgnUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SIgnUpView.swift
+//  Nectar
+//
+//  Created by students on 4/11/46.
+//
+
+import SwiftUI
+
+struct SIgnUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SIgnUpView()
+}
