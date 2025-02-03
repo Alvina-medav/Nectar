fba2a0e5c51f8e749ae2040125deb881df8f791a
Alvina Medav <alvinamed25@gmail.com>
2025-02-03 17:24:13 +0530

    Initial Commit

---
+++ Nectar/NectarApp.swift
@@ -0,0 +1,17 @@
+//
+//  NectarApp.swift
+//  Nectar
+//
+//  Created by alvina medav  on 03/02/25.
+//
+
+import SwiftUI
+
+@main
+struct NectarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
